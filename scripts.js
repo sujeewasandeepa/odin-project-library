ddd94de41b90f9daf7ef478250bdb21ded1fce80
Jayantha Kularathna <sujeewasandeepa@protonmail.ch>
2022-03-11 12:26:56 +0530

    attempting stuff

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const show_books = document.querySelector("#showBooks")
+
 let myLibrary = [
 {
     title : "Harry Potter",
@@ -12,17 +14,26 @@ let myLibrary = [
     available : true
 }]
 
-function Book () {
-    this.title = ''
-    this.author = ''
-    this.pages = 0
-    this.available = false
+function Book (title, author, pages, available) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.available = available
 }
 
-function addBookToLibrary (book) {
+function addBookToLibrary (title, author, pages, available) {
+    book = new Book(title, author, pages, available)
     myLibrary.push(book);
 }
 
 function viewLibrary () {
-    
+    myLibrary.forEach(book => {
+        console.log(book);
+
+        show_books.innerText = book.title
+    });
 }
+
+addBookToLibrary ("Hutta", "Pakaya", 100, false)
+
+viewLibrary()
